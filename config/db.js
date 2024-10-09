5ce1e1969992d679d058426f91044a1459ebe51e
Joan Paul <joanpauln@gmail.com>
2024-10-09 09:51:26 +0530

    Fix MongoDB warnings and syntax errors

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        };
         console.log('MongoDB connected');
     } catch (error) {
         console.error(`Error: ${error.message}`);
